e668a0e0610db3961882b4206ac0b5c35bf699a2
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-07-22 14:19:57 -0400

    dependencias y primer template

---
+++ src/Components/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Login() {
+  return (
+      <React.Fragment>
+  <button className="btn-primary">login</button>
+  </React.Fragment>
+  );
+}
+
+export default Login
