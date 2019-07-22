4d830271bcedb23ccc19e43beeb8e38a05b507c4
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-07-22 17:06:27 -0400

    home, login

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import { BrowserRouter , Route, Switch } from 'react-router-dom';
-import Login from './Components/login'
+import Login from './Components/login';
+import  Home from './Components/home';
 
 
 function App() {
   return (
   <BrowserRouter>
     <Switch>
-    <Route exact path="/" component={Login}/>
+    <Route exact path="/" component={Home}/>
+    <Route exact path="/login" component={Login}/>
     
     </Switch>
   </BrowserRouter>
