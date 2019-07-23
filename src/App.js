059f3a79821374dab278d7168ab18cfa1c698641
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-07-23 12:37:04 -0400

    select regiones, dashboard, arreglo estilos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,19 @@ import { BrowserRouter , Route, Switch } from 'react-router-dom';
 import Login from './Components/login';
 import Register from './Components/register';
 import  Home from './Components/home';
+import Dashboard from './Components/dashboard'
+
 
 
 function App() {
   return (
   <BrowserRouter>
     <Switch>
+      
     <Route exact path="/" component={Home}/>
     <Route exact path="/login" component={Login}/>
     <Route exact path="/register" component={Register}/>
+    <Route exact path="/dashboard" component={Dashboard}/>
     
     </Switch>
   </BrowserRouter>
