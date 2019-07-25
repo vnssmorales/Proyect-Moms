9e59618d4166293c3a11ad44b75acc03ac625407
josefin-ferrada <josefin.ferrada95@gmail.com>
2019-07-24 21:57:24 -0400

    reparado nav

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -26,14 +26,21 @@ class Dashboard extends Component {
         if(name == "publish"){
             this.setState({
                 showComponentPublish: true,
+                showComponentPublications: false,
+                showComponentProfile: false,
+                
             })
         }else if(name == "publications"){
             this.setState({
                 showComponentPublications: true,
+                showComponentPublish: false,
+                showComponentProfile: false,
             })
         }else if(name == "profile"){
             this.setState({
                 showComponentProfile: true,
+                showComponentPublish: false,
+                showComponentPublications: false,
             })
         }
         
@@ -59,35 +66,36 @@ class Dashboard extends Component {
     render() {
   return (
       <React.Fragment>
-         <Nav className="navbar fixed-top navbar-expand-md navbar-dark bg-primary mb-3">
-        <div className="flex-row d-flex">
-            <button type="button" className="navbar-toggler mr-2 " data-toggle="offcanvas" title="Toggle responsive left sidebar">
-                <span className="navbar-toggler-icon"></span>
+         <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-primary mb-3">
+            <div class="flex-row d-flex">
+                <button type="button" class="navbar-toggler mr-2 " data-toggle="offcanvas" title="Toggle responsive left sidebar">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#" title="Free Bootstrap 4 Admin Template">Admin Template</a>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
+                <span class="navbar-toggler-icon"></span>
             </button>
-            <a className="navbar-brand" href="/#" title="Free Bootstrap 4 Admin Template">hola</a>
-        </div>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="navbar-collapse collapse" id="collapsingNavbar">
-            <ul className="navbar-nav">
-                <li className="nav-item active">
-                    <a className="nav-link" href="/#">Home <span className="sr-only">Home</span></a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="//www.codeply.com">Link</a>
-                </li>
-            </ul>
-            <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                    <a className="nav-link" href="#myAlert" data-toggle="collapse">Alert</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/#" data-target="#myModal" data-toggle="modal">About</a>
-                </li>
-            </ul>
-        </div>
-    </Nav>
+            <div class="navbar-collapse collapse" id="collapsingNavbar">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">Home</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="//www.codeply.com">Link</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#myAlert" data-toggle="collapse">Alert</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="" data-target="#myModal" data-toggle="modal">About</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
     <div className="container-fluid" id="main">
         <div className="row row-offcanvas row-offcanvas-left">
             <div className="col-md-3 col-lg-2 sidebar-offcanvas bg-light pl-0" id="sidebar" role="navigation">
