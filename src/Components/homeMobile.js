87da6c4fa8db39cd678f260c20f72bbd83cf7ec4
KarenQuezadaMejia <quezadakaren16@gmail.con>
2019-07-25 15:19:06 -0400

    modificacion de home

--- src/Components/homeMobile.js
+++ src/Components/homeMobile.js
@@ -29,14 +29,14 @@ function HomeMobile() {
                     <h1 className="home-intro__subtitle">Cientos de mujeres y empresas confían en nosotros</h1>
                     <h1 className="home-intro__title home-intro__title--playfair">Te ayudamos a cubrir puestos de trabajo según tus necesidades.</h1>
                     </div>
-                    <Link to="/register"> <button type="button" className="btn btn-light m-2" id="ingresarEmp">Registra tu empresa</button></Link>
+                    <Link to="/register"> <button type="button" className="btn btn-light m-2" id="ingresarEmp">REGISTRA TU EMPRESA</button></Link>
                     </div>
                     </div>  
 
                    <div className="home-purpose">
                    <div className="home-heading__wrapper">
-                   <h1 className="home-heading__subtitle home-heading__subtitle--pink">Nuestro enfoque</h1>
-                   <h1 className="home-heading__title">Ten el talento que buscas según tu medida</h1>
+                   <p className="home-heading__subtitle home-heading__subtitle" id="enfoque">Nuestro enfoque</p>
+                   <h2 className="home-heading__title">Ten el talento que buscas según tu medida</h2>
                    <div className="home-heading__text">
                    
                    </div>
@@ -47,7 +47,7 @@ function HomeMobile() {
                    <div className="card" id="purpose">
                    <img src={image1} className="card-img-top" alt="..."/>
                    <div className="card-body">
-                   <h5 className="card-title">El mejor Fit</h5>
+                   <h3 className="card-title">El mejor Fit</h3>
                    <p className="card-text">Encuentra la candidata que más se ajuste a tus requerimientos específicos</p>
                  </div>
                  </div>
@@ -55,65 +55,21 @@ function HomeMobile() {
                  <div className="card" id="purpose3">
                    <img src={image3} className="card-img-top" alt="..."/>
                    <div className="card-body">
-                   <h5 className="card-title">Gestionamos</h5>
+                   <h3 className="card-title">Gestionamos</h3>
                    <p className="card-text">Te ayudamos a gestionar la cantidad de posibles postulantes según el tipo de trabajo: Part-time, Full-time o Proyectos individuales.</p>
                  </div>
                  </div>
                    </div>
                   
-                   <div className="home-services">
-                   <div className="home-heading">
-                   <div className="home-heading__wrapper">
-                   <h1 className="home-heading__title home-heading__title--white">Haz un impacto positivo<br/></h1>
-                   <div className="home-heading__text home-heading__text--white">Podrás pertenecer al grupo de compañías que promueven la igualdad de género y la inclusión laboral de mujeres<br/>
-                   <button type="button" className="btn btn-light m-2" id="aprende">Aprende más aquí</button>
-                  </div>
-                   
-                   </div>
-                   </div>
-
-                   <div className="home-services row justify-content-around">
-                   <div className="service-block col col-lg-3">
-                   <div className="service-block__wrapper">
-                   <div className="service-block__heading">
-                   <h1 className="service-block__title service-block__title--pink">Trabajos por proyecto</h1>
-                   </div>
-                   <div className="service-block__info">
-                   <h3 className="service-block__subtitle">Contrata a una mamá experta por tiempo acotado y con un objetivo claro</h3>
-                   <div className="service-block__text">Podemos entregarte a las mujeres más talentosas del mercado para desarrollar los proyectos que necesitas. Conoce a este grupo de mamás expertas y flexibles que adecuarán sus horarios para cubrir las necesidades de tu negocio.
-                   </div>
-                   </div>
-                   </div>
-                   </div>
-
-                   <div className="service-block col col-lg-3">
-                   <div className="service-block__wrapper">
-                   <div className="service-block__heading">
-                   <h1 className="service-block__title service-block__title--pink">Trabajos <br/>Part-time</h1>
-                   </div>
-                   <div className="service-block__info">
-                   <h3 className="service-block__subtitle">Contrata mamás por menos horas a la semana</h3>
-                   <div className="service-block__text">Mamás expertas pueden trabajar por menos horas a la semana y tener los resultados que tu negocio necesita. Este grupo quiere un trabajo con disponibilidad en oficina de 20 a 30 horas por semana.
-                   </div>
-                   </div>
-                   </div>
-                   </div>
-
-                   <div className="service-block col-lg-3">
-                   <div className="service-block__wrapper">
-                   <div className="service-block__heading">
-                   <h1 className="service-block__title service-block__title--pink">Full-time con Flexibilidad</h1>
-                   </div>
-                   <div className="service-block__info">
-                   <h3 className="service-block__subtitle">Contrata profesionales por tiempo completo, pero con algún tipo de flexibilidad<br/></h3>
-                   <div className="service-block__text">Tenemos Muchas mamás que quieren trabajar tiempo completo, pero no 100% desde una oficina o en horarios tradicionales. Puedes contratar mamás que quieren entre un 10% y 50% de flexibilidad para trabajar remoto (Teletrabajo) o buscan horarios diferentes o flexibles.
-                   </div>
-                   </div>
-                   </div>
-                   </div>
-                  
-                   </div>
-                   </div>
+                   <div className="home-intro" id="container2">
+                    <div className="home-intro__wrapper">
+                    <div className="home-intro__block">
+                    <h1 id="impact">Haz un impacto positivo</h1>
+                    <h5 className="home-intro__title home-intro__title" id="secondtext">Podrás pertenecer al grupo de compañias que promueven la igualdad de género y la inclusión laboral de mujeres</h5>
+                    </div>
+                    <button type="button" className="btn btn-light m-2" id="learnmore">APRENDE MÁS AQUÍ</button>
+                    </div>
+                    </div>  
 
                    <div className="web-clients">
                    <div className="grid-x">
@@ -122,7 +78,7 @@ function HomeMobile() {
                    </div>
                    </div>
 
-                   <div className="grid-x ">
+                   <div className="grid-x" id="footermob">
                    <div className="grid-column col col-2">
                    <img src={image6} alt="ey"/>
                    </div>
@@ -150,11 +106,9 @@ function HomeMobile() {
                    <div className="grid-x justify-content-around">
                    <div className="grid-column col col-lg-3" id="footer-news">
                    <h6 className="footer-newsletter__title">Suscríbete a nuestro Newsletter</h6>
-                   <div className="footer-newsletter__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ut sodales turpis.
-                   </div>
                    </div>
 
-                   <div className="grid-column col col-lg-9">
+                   <div className="grid-column col col-sm-6">
                    <div className="footer-form form">
                    <form id="email-form" name="email-form" data-name="Email Form">
                    <div className="footer-form__wrapper">
@@ -185,7 +139,7 @@ function HomeMobile() {
                    </div>
                    </div>
 
-                   <div className="grid-x justify-content-around">
+                   <div className="grid-x justify-content-around" id="bigfooter">
                    <div className="grid-column col col-3">
                    <img src={logo} id="logo-footer" alt="..."/>
                    <div className="footer-description">This is a digital platform and community that connect female talent with the very best companies that have this flexible offers for them. We are a trusted careers resource for women and a trusted talent resource for companies.
